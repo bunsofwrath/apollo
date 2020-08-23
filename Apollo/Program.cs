d8f63635d43107ae53d9ba3905db0e5fa6de99a2
bunsofwrath <ben.rathbun@gmail.com>
2020-08-23 01:10:48 -0500

    Added a lot of initial functionality to define tags, their association with files, and the file storage used to persist app data.

--- Apollo/Program.cs
+++ Apollo/Program.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace Apollo
 {
     class Program
     {
+        private static readonly string _dataFileName = ".apollo";
+
+        static IConfigurationRoot Configuration
+            => new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var folders = GetDirectories();
+            var taggedFiles = GetTaggedFiles(folders);
+
+
+            
+            var taggedFileData = GetTagData(taggedFiles);
+
+            foreach (var data in taggedFileData)
+                WriteDataFile(data.Key, data.Json);
         }
+
+        private static IEnumerable<string> GetDirectories()
+            => Configuration
+                .GetSection("directories")
+                .AsEnumerable()
+                .Skip(1)
+                .Select(f => f.Value);
+
+        private static IEnumerable<TaggedFile> GetTaggedFiles(IEnumerable<string> folders)
+            => folders
+                .Where(d => Directory.Exists(d))
+                .Select(d => new
+                {
+                    Directory = d,
+                    DataFileName = Path.Combine(d, _dataFileName)
+                })
+                .GroupBy(d => File.Exists(d.DataFileName))
+                .SelectMany(g => g.Key
+                    ? g.SelectMany(d => ReadTagDataFromFile(d.DataFileName))
+                    : g.SelectMany(d => BuildNewTagData(d.Directory)));
+
+        private static IEnumerable<TaggedFile> ReadTagDataFromFile(string fileName)
+            => JsonSerializer.Deserialize<IEnumerable<TaggedFile>>(File.ReadAllText(fileName));
+
+        private static IEnumerable<TaggedFile> BuildNewTagData(string directory)
+            => Directory.EnumerateFiles(
+                    directory,
+                    "*",
+                    new EnumerationOptions()
+                    {
+                        RecurseSubdirectories = true
+                    })
+                .Select(f => new TaggedFile(f, directory));
+
+        private static IEnumerable<(string Key, string Json)> GetTagData(IEnumerable<TaggedFile> taggedFiles)
+            => taggedFiles
+                .GroupBy(f => f.TopLevelDirectory)
+                .Select(g =>
+                (
+                    g.Key,
+                    Json: JsonSerializer.Serialize(g.ToList())
+                ));
+
+        private static void WriteDataFile(string folder, string json)
+            => File.WriteAllText(
+                Path.Combine(folder, _dataFileName),
+                json);
     }
 }
---
+++ Apollo/TaggedFile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Apollo
+{
+    [DebuggerDisplay("{FileName}")]
+    internal class TaggedFile
+    {
+        public byte[] MD5Hash { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string TopLevelDirectory { get; set; }
+
+        private string FileName // used only for debugger display
+            => Path.GetFileName(FilePath);
+
+        private IEnumerable<Tag> _tags;
+        public IEnumerable<Tag> Tags
+        {
+            get => _tags.ToList();
+            set => _tags = value;
+        }
+
+        public TaggedFile() // support deserialization from JSON
+        { }
+
+        public TaggedFile(string filePath, string topLevelDirectory)
+        {
+            // TODO: validate topLevelFolder against filePath
+
+            FilePath = filePath;
+            TopLevelDirectory = topLevelDirectory;
+
+            using (var md5 = MD5.Create())
+            using (var stream = File.OpenRead(FilePath))
+                MD5Hash = md5.ComputeHash(stream);
+
+            _tags = new List<Tag>();
+        }
+
+        private TaggedFile(string filePath, string topLevelDirectory, byte[] md5Hash, IEnumerable<Tag> tags)
+        {
+            FilePath = filePath;
+            TopLevelDirectory = topLevelDirectory;
+            MD5Hash = md5Hash;
+            _tags = tags;
+        }
+
+        public TaggedFile AddTag(Tag tag)
+            => new TaggedFile(FilePath, TopLevelDirectory, MD5Hash, _tags.Append(tag));
+    }
+}
